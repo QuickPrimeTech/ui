0c6bad196325edcb33190e0e5adc660c66168061
QuickPrimeTech <quickprimetech@gmail.com>
2026-04-09 18:54:52 +0300

    setup the inital instance of the project

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,11 +1,17 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { appName, gitConfig } from "./shared";
+import { Logo } from "@/components/logo";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: (
+        <>
+          <Logo className="bg-primary size-5 rounded-full text-primary-foreground" />
+          {appName}
+        </>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
